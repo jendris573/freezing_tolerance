914cfa6c733afed7580ef344b5d81ebfa3a6c1da
Joe Endris <jendris@my.apsu.edu>
2023-05-05 11:16:28 -0500

    Update after meeting

--- freezing_tolerance_stats.R
+++ freezing_tolerance_stats.R
@@ -4,8 +4,12 @@ library(readxl)
 library(fitdistrplus)
 library(lubridate)
 library(MuMIn)
+library(dplyr)
+library(pracma)
 
 LT50_data<-read_excel("~/Library/CloudStorage/GoogleDrive-jendris@my.apsu.edu/.shortcut-targets-by-id/1p5eHgH8eX9-QjkyyA3uRz5Lk7ontMZtO/Rehm lab - General/Trees/1- Freezing/Data/LT50 master.xlsx")
+
+LT50_data$Species <- as.factor(LT50_data$Species)
 
 LT50_data <- filter(LT50_data, State=="TN")
 
@@ -20,20 +24,56 @@ hist(LT50_data$LT50)
 #look at distribution of LT50s without considering treatment type
 descdist(LT50_data$LT50,discrete=FALSE)
 
+#force any LT50 values below -11 to be treated as -11
+LT50_data$LT50mod <- ifelse(LT50_data$LT50< -11, -11, LT50_data$LT50)
+
+hist(LT50_data$LT50mod)
+
+#look at distribution of LT50mods without considering treatment type
+descdist(LT50_data$LT50,discrete=FALSE)
+
 mod1<-glm(LT50~Species*julian_date*year,data=LT50_data,na.action="na.fail")
 summary(mod1)
+dredge(mod1)
 
-mod2<-glm(LT50~(Species+julian_date+year)^2,data=LT50_data)
+mod2<-glm(LT50mod~(Species+julian_date+year)^2,data=LT50_data)
 summary(mod2)
 
-mod3<-glm(LT50~Species*julian_date+Species*year,data=LT50_data)
+mod3<-glm(LT50mod~Species*julian_date+Species*year,data=LT50_data)
 summary(mod3)
 
 mod4<-glm(LT50~Species+julian_date+year,data=LT50_data)
 summary(mod4)
 
-dredge(mod1)
+summary(glht(mod4, mcp(Species="Tukey")))
 
 mod5<- glm(LT50~Species*julian_date+year, data=LT50_data)
 summary(mod5)
+
+
+
+
+
+#convert LT50mod to positive integers
+LT50_data$LT50mod2 <- LT50_data$LT50mod + 11.1
+LT50_data$LT50mod2 <- log(LT50_data$LT50mod2)
+
+mod1b<-glm(LT50mod2~Species*julian_date*year,data=LT50_data,na.action="na.fail", family=Gamma)
+summary(mod1b)
+
+dredge(mod1b)  
+
+descdist(LT50_data$LT50mod2,discrete=FALSE)
+hist(LT50_data$LT50mod2, breaks=20)
+
+
+###transform data####
+
+#create new column to create square root for LT50
+LT50_data$LT50trans <- LT50_data$LT50mod/mean(LT50_data$LT50mod)
+
+hist(LT50_data$LT50trans)
+
+
+
 
